ab7b70a4be8b4ec4774e7ac346361e52138cbbd7
siddhu-1805 <sravansiddhu349@gmail.com>
2024-07-12 17:20:28 +0530

    Add files via upload

---
+++ RelationalOperators.java
@@ -0,0 +1,18 @@
+package com.vasdoltechnologies;
+
+public class RelationalOperators {
+
+	public static void main(String[] args) {
+		//RelationalOperators
+		int a=30;
+		int b=20;
+		
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a>=b);
+		System.out.println(a<=b);
+		System.out.println(a!=b);
+		System.out.println(a==b);
+	}
+
+}
