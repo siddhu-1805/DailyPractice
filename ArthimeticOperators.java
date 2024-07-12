9ae429c857a3a532fb7ea50f98d85e9db2357d2d
siddhu-1805 <sravansiddhu349@gmail.com>
2024-07-12 12:14:04 +0530

    Add files via upload

---
+++ ArthimeticOperators.java
@@ -0,0 +1,22 @@
+package com.vasdoltechnologies;
+
+public class ArthimeticOperators {
+
+	public static void main(String[] args) {
+		//usage of arthimetic operations
+		int a=28;
+		int b=20;
+		System.out.println("Addition: "+(a+b));
+		System.out.println("Subtraction : "+(a-b));
+		System.out.println("Mutltiplication: "+(a*b));
+		System.out.println("Divivsion: "+(a/b));
+		System.out.println("Modulo : "+(a%b));
+		System.out.println("pre increment: "+ ++a);
+		System.out.println("post increment: "+ a++);
+		System.out.println("pre decrement: "+ --b);
+		System.out.println("post decrement: "+ b--);
+		System.out.println("After post decrement value of b is:"+b);
+
+	}
+
+}
