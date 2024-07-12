8c9bb9f868431e6eb7cd64addc7371ff5fb14af1
siddhu-1805 <sravansiddhu349@gmail.com>
2024-07-12 14:33:50 +0530

    Add files via upload

---
+++ AssignmentOperator.java
@@ -0,0 +1,19 @@
+package com.vasdoltechnologies;
+
+public class AssignmentOperator {
+
+	public static void main(String[] args) {
+		//Assignment Operators
+		int a = 23;
+		int b =18;
+		System.out.println(a+=b);
+		System.out.println(a-=b);
+		System.out.println(a*=b);
+		System.out.println(a/=b);
+		System.out.println(a%=b);
+		System.out.println(a&=b);
+		System.out.println(a|=b);
+		System.out.println(a^=b);
+	}
+
+}
