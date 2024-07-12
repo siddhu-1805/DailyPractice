095f062e9d8f98d1a35d316af156e15b68f9f995
siddhu-1805 <sravansiddhu349@gmail.com>
2024-07-12 17:20:52 +0530

    Add files via upload

---
+++ BitwiseOperators.java
@@ -0,0 +1,18 @@
+package com.vasdoltechnologies;
+
+public class BitwiseOperators {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		
+		System.out.println(a&b);
+		
+		System.out.println(a|b);
+		
+		System.out.println(a^b);
+		
+		System.out.println(~a);
+	}
+
+}
