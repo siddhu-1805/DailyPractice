7e40ce1b0af8aa6cbf8a788e3d000d7f41a49055
siddhu-1805 <sravansiddhu349@gmail.com>
2024-07-12 15:30:08 +0530

    Add files via upload

---
+++ LogicalOperators.java
@@ -0,0 +1,19 @@
+package com.vasdoltechnologies;
+
+public class LogicalOperators {
+
+	public static void main(String[] args) {
+		//logical operators
+		int a=200;
+		int b=150;
+		int c=100;
+		
+		System.out.println(a>b && b>c);
+		
+		System.out.println(a<b && c<a);
+		
+		System.out.println(a<b ||b>c);
+		
+	}
+
+}
